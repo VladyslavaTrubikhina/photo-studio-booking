cb7dbde612a6783502d882885b0a2f95dc52c275
lalavla <546003@student.saxion.nl>
2025-10-24 17:57:45 +0200

    Removed Example page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,7 +2,6 @@
     import router from "page";
     import Login from "./pages/Login.svelte";
     import Home from "./pages/Home.svelte";
-    import ExampleWithParams from "./pages/ExampleWithParams.svelte";
     import MyReservations from "./pages/MyReservations.svelte";
     import Profile from "./pages/Profile.svelte";
     import Header from "./lib/Header.svelte";
@@ -32,13 +31,6 @@
         Page = Profile;
     });
 
-    router("/example/:id", (ctx) => {
-        Page = ExampleWithParams;
-        currentRoute = ctx.pathname;
-        // The entire context is added. It contains the parameters
-        // as well as a wealth of information about the request.
-        context = ctx;
-    });
 
     router("/");
     router.start();
@@ -49,7 +41,6 @@
         {route: "/", name: "Home"},
         {route: "/reservations", name: "My reservations"},
         {route: "/users", name: "Profile"},
-        {route: "/example/testValue?key1=value1&key2=value2", name: "Example"},
     ]}/>
 {/if}
 <Page {context}/>
